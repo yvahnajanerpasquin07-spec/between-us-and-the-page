5f41ae28944ddc08601c1d9a102808edadb330c5
yvahnajanerpasquin07-spec <yvahnajanerpasquin07@gmail.com>
2026-09-03 17:26:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/between-us-and-the-page/',
+
   server: {
     port: 5173,
   },
